62694977d257e92e99a44a2bb237ba3d179f18ec
Aung T. T. Kyaw <zatlite@gmail.com>
2019-08-04 13:06:14 +1000

    added date, allOf, and someOf fields.

--- src/index.js
+++ src/index.js
@@ -18,8 +18,12 @@ type Data {
     sampleIntSize(list: [Int], size: Int!): [Int]
     currency(min: Float = 0, max: Float = 9999.99, allowNulls: Boolean = false): Float
     integer(min: Int = 0, max: Int = 9999, allowNulls: Boolean = false): Int
+    date(min: String = null, max: String = null): String!
     child(allowNulls: Boolean = false): Data
     children(max: Int!, allowEmpty: Boolean = false): [Data]
+    listItem: String!
+    allOf(list:[String!]!): [Data]!
+    someOf(list:[String!]!, max: Int = 0, allowEmpty: Boolean = false): [Data]!
 }
 `
 
@@ -38,9 +42,37 @@ const resolvers = {
         sampleIntSize: (_, args) => lo.sampleSize(args.list, args.size),
         currency: (_, args) => args.allowNulls && !randInt(0, 2) ? null: randCurrency(args.min, args.max),
         integer: (_, args) => args.allowNulls && !randInt(0, 2) ? null: randInt(args.min, args.max),
+        date: (_, args) => getDateString(args.min, args.max),
         child: (_, args) => args.allowNulls && !randInt(0, 2) ? null: {},
         children: (_, args) => randObjArray(args.max, args.allowEmpty),
+        listItem: (parent, args) => parent.listItem,
+        allOf: (_, args) => getAllOfObjArray(args.list),
+        someOf: (_, args) => getSomeOfObjArray(args.list, args.max, args.allowEmpty),
     }
+}
+
+function getDateString(min, max) {
+    var minIsDate = Date.parse(min);
+    var maxIsDate = Date.parse(max);
+    var minDate = new Date(min);
+    var maxDate = new Date(max);
+    var dayRange = 365*30;
+    if (!minIsDate && !maxIsDate) return new Date().toJSON();
+    else if (!minIsDate) return maxDate.addDays(- randInt(0, dayRange)).toJSON();
+    else if (!maxIsDate) return minDate.addDays(randInt(0, dayRange)).toJSON();
+    else return minDate.addDays(randInt(0, datediff(minDate, maxDate))).toJSON();
+}
+
+Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+function datediff(first, second) {
+    // Take the difference between the dates and divide by milliseconds per day.
+    // Round to nearest whole number to deal with DST.
+    return Math.round((second-first)/(1000*60*60*24));
 }
 
 /**
@@ -65,6 +97,19 @@ function randomString(len, an){
       str += String.fromCharCode(r+=r>9?r<36?55:61:48);
     }
     return str;
+}
+
+function getSomeOfObjArray(list, max, allowEmpty) {
+    if (allowEmpty && Math.random() > .5)
+    {
+        return [];
+    }
+    var size = max <= 0 ? list.length : max;
+    return getAllOfObjArray(lo.sampleSize(list, randInt(1, size)));
+}
+
+function getAllOfObjArray(list) {
+    return list.map((x, i) => { return { id: i, listItem: x } } );
 }
 
 function getObjArray(size) {
@@ -95,8 +140,13 @@ var names = [];
 axios.all([
     axios.get('https://uinames.com/api/?amount=50&region=australia'),
     axios.get('https://uinames.com/api/?amount=50&region=germany'),
-]).then(axios.spread((aus, ger) => {
+])
+.then(axios.spread((aus, ger) => {
     names = [...aus.data, ...ger.data];
-    server.start(() => console.log('Server is running on http://localhost:4000'))
-}));
+    server.start(() => console.log('Server is running on http://localhost:4000'));
+}))
+.catch(() => {
+    names = [{name: 'John', surname: 'Citizen'}, {name: 'Alan', surname: 'Turing'}];
+    server.start(() => console.log('Server is running on http://localhost:4000'));
+});
 
